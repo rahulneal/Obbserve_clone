65e92b3f189f4ce19c2e9ad08128382ded5c88e4
rahulneal <takrahul1440@gmail.com>
2021-11-01 18:16:54 +0530

    feat:like btn
    <audio> and <like> function added

---
+++ script.js
@@ -0,0 +1,50 @@
+const likebtn=document.querySelector("#love");
+const output=document.querySelector(".output");
+
+var x=0;
+function count(){
+     x++;
+     output.innerHTML=x;
+     console.log(x);
+}
+
+likebtn.addEventListener("click",count)
+
+
+const likebtn2=document.querySelector("#love2");
+const output2=document.querySelector(".output2");
+
+var y=0;
+function count2(){
+     y++;
+     output2.innerHTML=y;
+     console.log(y);
+}
+
+likebtn2.addEventListener("click",count2)
+
+
+const likebtn3=document.querySelector("#love3");
+const output3=document.querySelector(".output3");
+
+var z=0;
+function count3(){
+     z++;
+     output3.innerHTML=z;
+     console.log(z);
+}
+
+likebtn3.addEventListener("click",count3)
+
+const likebtn4=document.querySelector("#love4");
+const output4=document.querySelector(".output4");
+
+var a=0;
+function count4(){
+     a++;
+     output4.innerHTML=a;
+     console.log(a);
+}
+
+likebtn4.addEventListener("click",count4)
+
